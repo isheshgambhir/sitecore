d54d8068d8fd807d8009f1f1d412d3d2c56bc78f
Ishesh Gambhir <isheshgambhir@gmail.com>
2024-03-14 12:14:14 +1100

    first commit

---
+++ src/main/java/com/rest/sitecore/SitecoreApplication.java
@@ -0,0 +1,13 @@
+package com.rest.sitecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitecoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitecoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rest/sitecore/SitecoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.sitecore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitecoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
