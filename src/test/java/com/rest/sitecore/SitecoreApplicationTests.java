63c7c1b5ba31b58d43825a0e4c7b3c0d8399556c
Ishesh Gambhir <isheshgambhir@gmail.com>
2024-03-14 13:55:19 +1100

    test(applicationTest): check application loads correctly

--- src/test/java/com/rest/sitecore/SitecoreApplicationTests.java
+++ src/test/java/com/rest/sitecore/SitecoreApplicationTests.java
@@ -1,13 +1,22 @@
 package com.rest.sitecore;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.rest.sitecore.controller.IssueController;
 
 @SpringBootTest
 class SitecoreApplicationTests {
 
+	@Autowired
+	private IssueController issueController;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(issueController);
 	}
 
 }
