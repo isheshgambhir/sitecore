e65a7ee8e2018eeecc79ed4e07847fd0debb7f6b
Ishesh Gambhir <isheshgambhir@gmail.com>
2024-03-14 13:02:40 +1100

    feat(issues): add issue entity class and repository

---
+++ src/main/java/com/rest/sitecore/model/Issue.java
@@ -0,0 +1,32 @@
+package com.rest.sitecore.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Issue {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+
+    String title;
+    String description;
+    
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/main/java/com/rest/sitecore/repository/IssueRepository.java
@@ -0,0 +1,11 @@
+package com.rest.sitecore.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.rest.sitecore.model.Issue;
+
+public interface IssueRepository extends CrudRepository<Issue, Long> {
+    Optional<Issue> findById(Long id);
+}
